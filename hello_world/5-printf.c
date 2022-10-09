513ccef38cc8862d1b838452bf8fb3e9a2fc0d3c
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-09 13:46:32 -0700

    Printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main - this prints only the specific text
+ * Description : This was the fuction we used
+ * to print the text that the excercise gave us
+ * Return: 0
+ */
+int main(){
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return 0;
+}
+ 

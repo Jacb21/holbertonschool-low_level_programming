82351ad34b4a224934c31c61434cc0946548ef15
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-09 13:55:09 -0700

    Printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,8 +5,9 @@
  * to print the text that the excercise gave us
  * Return: 0
  */
-int main(void) {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return 0;
 }
  

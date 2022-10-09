ce896f081dec2a1f81f9081215e628833b02c9e8
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-09 13:31:54 -0700

    Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void);
+/**
+ * main - this prints only the specific text
+ * Description This was the function we used to print the text that the exercise gave us
+ * Return: 0
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);

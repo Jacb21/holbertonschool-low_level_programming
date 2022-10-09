4632405d297e3c69f575aa2d42fbdf774a3ec8b6
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-09 13:34:11 -0700

    Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - this prints only the specific text
- * Description This was the function we used 
+ * Description This was the function we used
  * to print the text that the exercise gave us
  * Return: 0
  */

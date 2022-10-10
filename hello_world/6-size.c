d1583f061e33fb05b492bd8cb032887fbfd22336
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-09 18:27:11 -0700

    Size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-void main()
+/**
+ * main - print the text
+ * Description : this was the fuction we used
+ * to print the text that the excercise gave us
+ * Return : 0
+ */
+int main(void)
 {
-printf("%lu\n", sizeof(char));
-printf("%lu\n", sizeof(int));
-printf("%lu\n", sizeof(long int));
-printf("%lu\n", sizeof(long long));
-printf("%lu", sizeof(float);
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
+printf("Size of a float: %ld byte(s)", sizeof(float);
 return (0);
 }

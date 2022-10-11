e4dc0e2edcae48b87b3ef83093d1f5a3506e0ecd
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-11 08:02:06 -0700

    Positive Negativ git push Positive Negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@ int main(void);
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand(n+1) - RAND_MAX / 2;
         if(n < 0 positive);
 	printf("%d : is positive\n");
 	else(n >0, negative);

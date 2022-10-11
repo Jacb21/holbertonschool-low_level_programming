88d2ef7189ec09173f5b1fe55f59e84201c2719e
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-10 18:01:55 -0700

    Positive, Negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ */
+int main(void)
+	{
+        if(n < 0, positive);	{
+	printf("is positive\n");
+	}
+	else(n >0, negative);   {
+	printf("is negative\n");
+        }
+	return (0);
+	}
+

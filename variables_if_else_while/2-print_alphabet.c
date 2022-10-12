86fba4228af5462d76ffb5357c3e76943ab9c856
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-12 15:32:32 -0500

    Print Alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	for (int x = 'A', x <= 'Z', x++)
+	{
+		x = tolower(x);
+		putchar (x);
+	}
+	return (0);
+}

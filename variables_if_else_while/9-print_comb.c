8cbb32ad154fc4a2e9989e1b7c26c29a85e7b339
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-12 18:57:57 -0500

    Print Comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - print the text
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+			int n;
+			for (n = 0; n <= 9; n++)
+			{
+				putchar(n + '0');
+			}
+			putchar('\n');
+			return (0);
+}

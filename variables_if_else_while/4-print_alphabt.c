ac0aa210a214beb0e105c19cc6e89f339ce873ff
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-12 16:37:07 -0500

    Print

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+	* main - print the text
+	* Return: 0
+	*/
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q' && a != e)
+		putchar (a);
+	}
+	return (0);
+}

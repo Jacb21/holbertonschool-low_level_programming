18d1189e8d27f5e7694dfdcc2960ef8d71973f6f
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-12 16:14:12 -0500

    Print Alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+	* main - print the text
+  * Return: 0
+	*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int a,b;
+
+	for (a = 'a'; a <= 'z'; a++)
+	for (b = 'A'; b <= 'Z'; b++)
+	{
+		putchar(a);
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
+

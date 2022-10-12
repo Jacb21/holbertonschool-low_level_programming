27536d686fb6c13e6e4ff96319ee212ab8a39b5d
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-12 17:04:27 -0500

    Print Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+	* main - print the text
+	* Return: 0
+	*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+		int n;
+
+		for (n = 0; n <= 9; n++)
+		{
+			putchar(n);
+		}
+		return (0);
+}

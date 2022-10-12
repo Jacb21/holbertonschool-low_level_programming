ec35dafdbfd8f42d3d0162e66ac1625b5fcabbb0
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-12 18:44:34 -0500

    Print Base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+ * main - print the text
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+		int n;
+
+		for (n = 10; n <= 'A'; n++)
+		{
+			putchar(n);
+		}
+		putchar('\n');
+		return (0);
+}

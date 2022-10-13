6b734772019f096fa0ec1dca33670d0f57d244e6
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-12 22:32:46 -0500

    Print Comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,11 +10,12 @@
 int main(void)
 {
 			int n;
+
 			for (n = 0; n <= 9; n++)
 			{
 				putchar(n + '0');
-				putchar(',');
 				putchar(' ');
+				putchar(',');
 			}
 			putchar('\n');
 			return (0);

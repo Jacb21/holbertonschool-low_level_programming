39c104a1a9e9c777cb0bb6955d8aad2389831b53
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-13 09:22:23 -0500

    Putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/**
+	*main - print the text
+	*Return: 0
+	*/
+int main(void)
+{
+	int p;
+
+	for (p = '_putchar')
+	{
+		putchar(p);
+	}
+	putchar('\n');
+	return (0);
+}

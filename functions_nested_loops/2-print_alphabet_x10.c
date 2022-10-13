191b50a85c98457b86c56a8ed5039c4fd5742631
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-13 11:29:08 -0500

    Alphabet x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *print_alphabet_x10 - print the alphabet x10
+ *Description: us for print the alphabet 10 times
+ *Return: 0
+ */
+void print_alphabet_x10(void)
+{
+int x,t;
+t=0;
+while (t < 10)
+{
+	for (x = 'a'; x <= 'z'; x++)
+{
+	putchar(x);
+}
+putchar('\n');
+return: 0
+}
+}

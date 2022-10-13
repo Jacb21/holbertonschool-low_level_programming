f307eff4968b5b71cc8bf8e068f0517ea1951158
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-13 10:11:09 -0500

    Alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+#include <stdlib.h>
+#include <time.h>
+/**
+	*main - print the text
+	*Return: 0
+	*/
+int main(void)
+{
+	int x
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}

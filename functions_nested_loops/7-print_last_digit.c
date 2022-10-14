5a47be531041246b022ec51e23fa08aa021dbe37
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 14:54:29 -0500

    Print Last Digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "main.h"
 /**
- *print_last_digit - print last digit 
+ *print_last_digit - print last digit
  *@n: variable
  *Description: print sign
  *Return: 0
@@ -12,8 +12,9 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		n = n * -1; 
+		n = n * -1;
 	}
 	n = n % 10;
+	_putchar (n + '0');
 	return (n);
 }

4f3be2b621e9ea68db44ff7f20949a322684d313
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 18:13:58 -0500

    Print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,21 +11,12 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
+		prinft("%d, ", n);
 		if (n > 98)
-		{
-			_putchar (n/10 + '0');
-			_putchar (n%10 + '0');
-			_putchar (',');
-			_putchar (' ');
 			n--;
-		}
-		else if (n < 98)
-		{
-			_putchar (n/10 + '0');
-			_putchar (n%10 + '0');
-			_putchar (',');
-			_putchar (' ');
+		else
 			n++;
-		}
 	}
+	if (n == 98)
+		printf("%d\n", n);
 }

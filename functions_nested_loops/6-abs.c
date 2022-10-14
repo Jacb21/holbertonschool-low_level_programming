6adb7ca6940f524be3f82486c21daef00e9156e6
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 13:59:44 -0500

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ *_abs - print absolute value
+ *print_sign - print sign
+ *@n: variable
+ *Description: print absolut value
+ *Return: 0
+ */
+int _abs(int n)
+{
+		_putchar ((n * -1)+'0');
+		return (0);
+}

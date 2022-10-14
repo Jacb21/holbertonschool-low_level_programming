927a597a1edae7072bc6459f614597b407c9b50f
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 15:26:36 -0500

    24 HOURS

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+	*print_last_digit - print last digit
+	*@n: variable
+	*Description: print sign
+	*Return: 0
+	*/
+void jack_bauer(void)
+{
+	int n;
+
+	for (n >= 00:00, n <=23:59, n++)  
+	{
+		_putchar (n + '0');
+	}
+		return (0);
+}

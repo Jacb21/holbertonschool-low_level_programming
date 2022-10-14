67f2cee52f80adc147e783b4dc5bd800a0931e9e
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 15:44:09 -0500

    24 HOURS

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,9 +13,9 @@ void jack_bauer(void)
 	int hour;
 	int minute;
 
-	for (hour = 0, hour = 23, hour++)  
+	for (hour = 0; hour = 23; hour++)  
 	{
-		for (minute = 0, minute = 59, minute++)
+		for (minute = 0; minute = 59; minute++)
 			_putchar (hour + '0');
 			_putchar (':');
 			_putchar (minute + '0');

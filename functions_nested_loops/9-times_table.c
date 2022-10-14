8f953436595de5a6ca7ccd29877d6d533446249d
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 16:59:59 -0500

    Time Table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,9 +18,22 @@ void times_table(void)
 		for (columna_x9 = 0; columna_x9 <= 9; columna_x9++)
 		{
 			result_x9 = fila_x9 * columna_x9;
+			if (columna_x9 == 0)
+			{
+				_putchar (columna_x9 + '0');
+			}
+			_putchar (',');
+			_putchar (' ');
 			if (result_x9 >= 10)
-			_putchar ((result_x9 / 10) +'0');
-			_putchar ((result_x9 % 10) + '0');
+			{
+				_putchar ((result_x9 / 10) + '0');
+				_putchar ((result_x9 % 10) + '0');
+				
+			}
+			else if (result_x9 <= 9)
+			{
+				_putchar (result_x9 + '0');
+			}
 		}
 	}
 }

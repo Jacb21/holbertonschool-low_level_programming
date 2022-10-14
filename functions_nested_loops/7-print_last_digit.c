54ce9f396629583871abdab85ae22374dc78d9f6
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 15:02:28 -0500

    Print Last Digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -13,8 +13,14 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		n = n * -1;
+		n = n % 10;
+		_putchar (n + '0');
+		return (n);
 	}
-	n = n % 10;
-	_putchar (n + '0');
-	return (n);
+	else
+	{
+		n = n % 10;
+		_putchar (n + '0');
+		return (n);
+	}
 }

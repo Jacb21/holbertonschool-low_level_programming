1584ce3ad16b0f57156a17a265112bebd74e82aa
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 17:15:35 -0500

    Add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ *add - add the numbers
+ *Description: add the numbers
+ *Return: 0
+ */
+int add(int a, int b)
+{
+	r = a + b
+	return (r);
+}

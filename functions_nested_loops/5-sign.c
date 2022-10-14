2f02c172ea6bf9eb71954c7f7ebb463c1a593ebc
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-13 21:59:36 -0500

    Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,9 +10,12 @@
 int print_sign(int n)
 {
 	if (n > 0)
+		putchar (n);
 			return (1);
 	else if (n == 0)
+		putchar (n);
 			return (0);
-	else
+	else if (n < 0)
+    putchar (n);
 		  return (-1);
 }

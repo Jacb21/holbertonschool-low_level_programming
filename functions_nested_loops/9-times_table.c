22e10e7b5ecfdb75454c08e8cec00dff4399bf70
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 16:20:22 -0500

    Time Table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ *jack_bauer - hours and minute
+ *Description: print sign
+ *Return: 0
+ */
+void times_table(void)
+{
+	int fila_x9;
+	int columna_x9
+
+	for (fila_x9 = 0; fila_x9 <= 9; fila_x9++)
+	{
+		for (columna_x9 = 0; columna_x9 <=9 colomuna_x9++)
+			_putchar (fila_x9);
+			_putchar (columna_x9);
+	}
+}

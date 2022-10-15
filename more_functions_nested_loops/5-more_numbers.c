2bca41408fe434e5257784782107965652db9c37
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-15 00:35:18 -0500

    More Numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-	*more_numbers - print more numbers 
+	*more_numbers - print more numbers
 	*Description: print more numbers 10 times
 	*Return: 0
 	*/
@@ -12,7 +12,7 @@ void more_numbers(void)
 
 	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = 0; num2 <=14; num2++)
+		for (num2 = 0; num2 <= 14; num2++)
 		{
 			if (num2 > 9)
 			{

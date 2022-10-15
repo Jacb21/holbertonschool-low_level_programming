8656ad55ce84053e143b4046eb1bdd7bddafad38
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-15 08:31:52 -0500

    More Numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,17 +10,12 @@ void more_numbers(void)
 	int num1;
 	int num2;
 
-	for (num1 = 0; num1 < 9; num1++)
+	for (num1 = 0; num1 < 14; num1++)
 	{
-		num2 = num1;
-		for (num2 = 0; num2 <= 14; num2++)
-		{
-			if (num2 > 9)
-			{
-				putchar ((num2 / 10) + '0');
-				putchar ((num2 % 10) + '0');
-			}
-		}
-		putchar('\n');
+		if ( num1 > 9)
+			putchar ((num1 / 10)+ '0');
+			putchar ((num1 % 10)+ '0');
+		else
+			putchar (num1);
 	}
 }

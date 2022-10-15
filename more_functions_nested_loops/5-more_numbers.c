9b15732b9e32717710b48715477ada97676fe519
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-15 00:01:46 -0500

    More Numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+	*more_numbers - print more numbers 
+	*Description: print more numbers 10 times
+	*Return: 0
+	*/
+void more_numbers(void)
+{
+	int num, tim;
+	tim = 0;
+	while (tim < 10)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			putchar (num);
+		}
+		tim++;
+		putchar('\n');
+	}
+}

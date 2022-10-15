728211dc67a33034550f535072668c462d7e5fe7
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-15 00:32:18 -0500

    More Numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -16,10 +16,10 @@ void more_numbers(void)
 		{
 			if (num2 > 9)
 			{
-				putchar ((num2 / 10) + '0');
-				putchar ((num2 % 10) + '0');
+				_putchar ((num2 / 10) + '0');
+				_putchar ((num2 % 10) + '0');
 			}
-		}		
+		}
+		_putchar('\n');
 	}
-	putchar('\n');
 }

1a02df1b62fa838dd6f35df94600e72f0a7decc8
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-15 12:49:57 -0500

    Print Line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,14 +3,12 @@
 /**
 	*print_line - print the line
 	*Description: print the line
-	*#include <stdio.h>
-#include "main.h"
-/**
-*print_line - print the line
-*Description: print the line
-*#include <stdio.h>
-#include "main.h"
-/**
-*print_line - print the line
-*Description: print the line
-*
+	*Return: 0
+	*/
+void print_line(int n)
+{
+	int _;
+	n = _;
+	putchar (n);
+}
+

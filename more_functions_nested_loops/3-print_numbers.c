cc7130a43dafca4a1027687de358d5da41c52217
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 23:32:20 -0500

    Print Numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+	*print_numbers - print the numbers
+	*Description: print the number you need
+	*Return 0
+	*/
+void print_numbers(void)
+{
+	int numbers
+
+	if (numbers >= '0' && numbers <= '9')
+		_putchar (numbers)
+}

06c82e15cbc26169b2c339dbb11c62ae86d7c335
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-14 23:17:44 -0500

    Isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+	*_isdigit - print a one digit
+	*@c: variable
+	*Description: print a one digit
+	*Return: 1, 0
+	*/
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+
+	else
+		return (0);
+}

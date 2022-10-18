6ce46489a460ddcf8fb5c76ddb611b9c36b6f1c8
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-18 18:36:46 -0500

    Swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+ /**
+	 *swap_int - the function swap num
+	 *@a: Variable
+	 *@b: Variable
+	 *Description: the function swap num
+	 */
+void swap_int(int *a, int *b)
+{
+	int num;
+	num = *a;
+
+	a = b*;
+	b = num;
+}

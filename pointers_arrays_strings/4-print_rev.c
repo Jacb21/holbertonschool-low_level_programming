f48f7716ae4bdb601e6254fe98a5bf95a80b76f0
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-19 06:06:20 -0500

    Print Rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+	*_strlen - print length of string
+	*@s: Variable
+	*Description: print length of string
+	*Return: 0
+	*/
+int _strlen(char *s)
+{
+	int log;
+
+	for (log = 0; s[log] != '\0'; loo++);
+	return (log);
+}
+
+/**
+	*print_rev - print the text in reverse
+	*@s: Variable
+	*Description: print the text in reverse
+	*/
+void print_rev(char *s)
+{
+	for rev;
+
+	for (rev = _strlen(s) - 1; rev >= 0; rev--)
+	{
+		_putchar(s[rev]);
+	}
+	_putchar('\n');
+}

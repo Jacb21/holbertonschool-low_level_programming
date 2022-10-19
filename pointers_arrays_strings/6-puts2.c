3f9a8e01d4b7f3542411e553672b8ecbf294b0a9
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-19 11:16:04 -0500

    Puts 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,10 @@
 void puts2(char *str)
 {
 	int tex, log;
-	{
-	for (log = 0; str[log] != '\0'; log++)
-	}
+
+	for (log = 0; str[log] != '\0'; log ++)
+	{}
+
 	for (tex = 0; str[tex] != '\0'; tex += 2)
 	{
 		_putchar(str[tex]);

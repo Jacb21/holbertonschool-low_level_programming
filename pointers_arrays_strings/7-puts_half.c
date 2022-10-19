38929697cf079153f0d303f98a237898be03cc9c
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-19 14:19:22 -0500

    Puts Half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+	*puts_half - print the medium text
+	*@str: Variable 
+	*Description: print the medium text
+	*/
+void puts_half(char *str)
+{
+	int log, tex, n;
+	n = (log - 1) / 2;
+
+	for (log = 0; str[log] != 0; log++)
+
+	for (tex = 0; tex > log/2; tex++)
+		
+		if (log % 2 == 0);
+		{
+			_putchar(str[tex]);
+		}
+		else
+		{
+			_putchar(n);
+		}
+}

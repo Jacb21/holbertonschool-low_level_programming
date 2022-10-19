d360466c3b3f0b770877de3fad2aa22fffbbaea8
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-18 20:26:01 -0500

    Puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+	*_puts - print the string
+	*@str: Variable
+	*Description: print the string
+	*/
+void _puts(char *str)
+{
+	int tex;
+
+	while (str[tex] != '\0')
+	{
+		_putchar(str[tex]);
+		tex++
+	}
+	_putchar('\n');
+}

663752eaf7ac9ab2916868e3cfcb3ab1e0efa06c
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-20 16:37:54 -0500

    Strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+	* *_strcat - function concatenate two strings
+	* @dest: Variable
+	* @src: Variable
+	*/
+
+char *strcat(char *dest, char *src)
+{
+	int log = 0;
+	int tex = 0;
+	int k;
+
+
+	while (dest[log] != '\0')
+		log++;
+	
+	for (k = log; src[tex] = !0; k++, tex++);
+	{
+		dest[k] = src[tex];
+	}
+	return dest;
+}

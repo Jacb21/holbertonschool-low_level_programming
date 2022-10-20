fa49b1c89a41206d9b13768a487a93e459244455
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-20 13:15:24 -0500

    Swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include "main.h"
  /**
-	 *swap_int - the function change the num
+	 *swap_i nt - the function change the num
 	 *@a: Variable
 	 *@b: Variable
 	 *Description: the function change the num
-	 *swap_int
 	 */
 void swap_int(int *a, int *b)
 {

04248c5c2ca35c042ea179d6d25a0791ad601c4f
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 17:13:32 -0500

    Cap String

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+	* *cap_string - function capitalizes all words of a string
+	* @c: Variable
+	* Return: m
+	*/
+
+char *cap_string(char *m)
+{
+	int w = 0;
+	int a;
+	char arr[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', ')', ')', '{', '}'};
+
+	if (m[0] >= 'a' && m[0] <= 'z')
+	{
+		m[0] -= 32;
+	}
+	while (m[w] != '\0')
+	{
+		if (m[w + 1] >= 97 && m[w] <= 122)
+		{
+			for (a = 0; arr[a] != '\0'; a++)
+			{
+				if (arr[a] == m[w])
+				{
+					m[w + 1] -= 32;
+					break;
+				}
+			}
+		}
+		w++;
+	}
+	return (m);
+}

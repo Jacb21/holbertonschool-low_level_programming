35e9765e2b3c470361a12556d707f240c5ae26db
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 08:56:08 -0500

    Strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+ /**
+	 * *_strncpy - function copy the text
+	 * @dest: Variable
+	 * @src: Variable
+	 * @n: Variable
+	 * Return: dest
+	 */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c = 0;
+
+	while (dest[c] != '\0' && c < n)
+	{
+		dest[c] = src[c];
+		c++;
+	}
+	while (c < n)
+	{
+		dest[c] = '\0';
+		c++;
+	}
+	return (dest);
+}

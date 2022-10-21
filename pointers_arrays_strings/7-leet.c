1501a66eb939a75a4e62b0271bb080dd34e55ee1
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 18:58:30 -0500

    Leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,13 +8,14 @@
 
 char *leet(char *e)
 {
-	int t = 0;
+	int t;
 	int w = 0;
 	char l[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L', '\0'};
 	char n[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1', '\0'};
 
 	while (e[t] != '\0')
 	{
+		t = 0;
 		while (l[w] != '\0')
 		{
 			if (e[t] == l[t])

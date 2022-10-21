710be8337d03483cf5216c8a59a894ee45a48d09
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 11:34:42 -0500

    Rev Array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+	* reverse_array - function that reverses the content
+	* @a: Variable
+	* @n: Variable
+	*/
+
+void reverse_array(int *a, int n)
+{
+	int c, t;
+	int aux;
+	c = n - 1;
+
+	for (t = 0; t <= c; t++, c--)
+	{
+		aux = a[t];
+		a[t] = a[c];
+		a[c] = aux;
+	}
+}

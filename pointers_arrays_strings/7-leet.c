251d88873a9e8e94d2cea10c7cb4bf3543021253
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 18:55:47 -0500

    Leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+	* *leet - function encodes a string into 1337
+	* @e: Variable
+	* Return: 0
+	*/
+
+char *leet(char *e)
+{
+	int t = 0;
+	int w = 0;
+	char l[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L', '\0'};
+	char n[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1', '\0'};
+
+	while (e[t] != '\0')
+	{
+		while (l[w] != '\0')
+		{
+			if (e[t] == l[t])
+			{
+				e[t] = n[w];
+			}
+			t++;
+		}
+		w++;
+	}
+	return (s);
+}

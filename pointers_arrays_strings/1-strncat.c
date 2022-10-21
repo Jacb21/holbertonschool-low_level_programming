9a9de232e19e30a330d8862baacba87c264510f3
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 08:07:37 -0500

    Strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+	* *_strncat - function 
+	* @dest: Variable
+	* @src: Variable
+	* @n: Variable
+	* Return: dest
+	*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int log = 0;
+	int k;
+
+	while (dest[log] != '\0')
+		log++;
+
+	for (k = log; src[n] != '\0'; n++, log++)
+	{
+		dest[log] = src[n];
+	}
+	return (dest);
+}

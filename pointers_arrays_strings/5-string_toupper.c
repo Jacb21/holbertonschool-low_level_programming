3349a6352d49e495356dcc11fc57d81e67fad2a1
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 14:41:57 -0500

    String Toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+	* *string_toupper - function change the letters to uppercase
+	* @
+	*/
+char *string_toupper(char *m);
+{
+	int t = 0;
+
+	for (m = 0; m[t] = '\0', m++)
+	{
+		if (m >= 'a' && m <= 'z')
+			m[t] = m[t] - 32;
+	}
+	return (m);
+}

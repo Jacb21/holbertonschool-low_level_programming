4dcafabf1625dda7df62becbf60a2a908b774d5e
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 21:23:39 -0500

    Strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+	* *_strstr -function locates a substring
+	* @haystack: string
+	* @needle: substring
+	* Return: 0
+	*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *string = haystack;
+		char *string2 = needle;
+		
+		while (*haystack && *string2 && *haystack == *string2)
+		{
+			haystack++;
+			string2++;
+		}
+		if (!*string2)
+		{
+			return (string)
+		}
+		haystack = string + 1;
+	}
+	return (0);
+}

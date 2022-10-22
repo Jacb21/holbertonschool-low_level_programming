79c0a07aa509bc093ac532d00551f752c9af9ccb
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 21:09:43 -0500

    Strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+	* *_strpbrk - function searches a string for any of a set of bytes
+	* @s: string1
+	* @accept: string2
+	* Return: 0
+	*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int t;
+	int b;
+
+	t = 0;
+
+	while (s[t])
+	{
+		b = 0;
+		while (accept[b])
+		{
+			if (s[t] == accept[b])
+			{
+				return (s + t);
+			}
+			b++
+		}
+		t++
+	}
+	return (0);
+}

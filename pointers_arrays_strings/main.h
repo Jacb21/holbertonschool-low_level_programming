6dfe412af9dc35331e73fd699f05f87797aea191
Jacb21 <cuellarbenitezj@gmail.com>
2022-10-21 19:41:49 -0500

    Prototype

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,4 +32,15 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 
+/**
+	*even more pointers, arrays and strings
+	*/
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+
 #endif
